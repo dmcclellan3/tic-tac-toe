1cfc5eb9de787f46ac99f6be20798f5c88f5bf7e
dmcclellan3 <wdmcclellan3@gmail.com>
2024-04-17 15:25:39 -0400

    saving

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
 // board for the game
-const board = [[" ", " " , " "],
-               [" ", " " , " "],
-               [" ", " " , " "],
-]
+// const board = [[" ", " " , " "],
+//                [" ", " " , " "],
+//                [" ", " " , " "],
+// ]
 
-const game = {  //object for the game
-    update : function(){}
-    isgameOver: function(){},
-    move : function(c){},
-    possibleMoves : function(){},
-    updateBoard : function(){
-        console.log("  ") //empty space for the board
-        board.forEach((arr, i) => {
-        console.log(arr.toString().replace(/,/g, "|"))
-        })
-    }
-}
+// const game = {  //object for the game
+//     update : function(){}
+//     isgameOver: function(){},
+//     move : function(c){},
+//     possibleMoves : function(){
+//         const p = []
+
+//         for(let i = 0; i < board.length; i++){
+//             for(let j = 0; j < board[0].length; j++){
+//                 if(board[i][j] === " "){
+//                     p.push({row: i, col: j})
+//                 }
+//             }
+//         }
+//         return p
+
+//         updateBoard : function(){
+//         console.log("  ") //empty space for the board
+//         board.forEach((arr, i) => {
+//         console.log(arr.toString().replace(/,/g, "|"))
+//         //changing an array to a string and replacing the commas with lines to configure the board
+//         })
+//         }
+ 
+//     }
+// }
