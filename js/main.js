840cf9081a4a4829ed696a44630ea1e5812c0066
dmcclellan3 <wdmcclellan3@gmail.com>
2024-04-18 16:37:20 -0400

    save

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 // Bringing over elements that are needed
 
 const cells = document.querySelectorAll('.cell')
-const restartBtn = document.querySelector('restart-button')
-const statusText = document.querySelector('status-text')
-document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', updateCell))
+const restartBtn = document.querySelector('#restart-button')
+const statusText = document.querySelector('#status-text')
+const boardArray = document.querySelectorAll('.cell')
+boardArray.forEach(cell => cell.addEventListener('click', updateCell))
 document.querySelector('.restart-button').addEventListener('click', restartGame) 
+console.log('boardArray', boardArray)
+
 
 // Listing out the items in the array for all possible ways to win. 
 
@@ -26,59 +29,69 @@ let running = true
 
 //all the functions needed for the game
 
-startGame()
-updateCell()
-checkForWinner()
-playerChange()
-restartGame()
+
 
 function startGame(){
 
     document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', updateCell))
     document.querySelector('restart-button').addEventListener('click', restartGame) 
     statusText.textContent = `${currentPlayer}s turn`
+    running = true 
 
 }
 
 
 
-//closest finding what element we are in.  Whatever was clicked give us the closest parent that has a cell class. 
+//closest finds what element we are in.  Whatever was clicked give us the closest parent that has a cell class. 
 function updateCell(event){
 
     const cell = event.target.closest('.cell')
-    cell.textContent = 'X'
-    console.log('cell', cell.getAttribute('data-cell-index'))
-    selection = currentPlayer
-}
-
-function checkForWinner(){
-    
-    let roundWin = false
-    for (let i = 0; i < winConditions.length; i++){
-        const condition = winConditions[i]
-        const cellA = selection[condition[0]]
-        const cellB = selection[condition[1]]
-        const cellC = selection[condition[2]]
+    if (cell.textContent === '') {
+        cell.textContent = currentPlayer
+        statusText.classList.remove("d-none")
         
-        if(cellA == '' || cellB == '' || cellC == ''){
-            continue;
-        }
-        if(cellA == cellB && cellB == cellC){
-        roundWin == true;
-        break;
-    }
-}
-    if(roundWin){
-        statusText.textContent = `${currentPlayer}wins!`
-        running = false
-    }
-    else if(!selection.includes('')){
-        statusText.textContent = 'Draw!'
-        running = false
-    }
-    else{
+        checkForWinner()
         playerChange()
     }
+    
+}
+    
+function checkForWinner(){
+    
+    let roundWon = false
+    for (let i = 0; i < winConditions.length; i++){
+        // const condition = winConditions[i]
+        const cellA = winConditions[i][0]
+        const cellB = winConditions[i][1]
+        const cellC = winConditions[i][2]
+
+        console.log('cellABCof ', i, cellA, cellB, cellC)
+        
+        if(boardArray[cellA].textContent === '' || boardArray[cellB].textContent === '' || boardArray[cellC].textContent === ''){
+            continue;
+        }
+        // if(cellA === cellB && cellB === cellC){
+            // console.log("BA ", boardArray[cellA].textContent, boardArray[cellB].textContent, boardArray[cellC].textContent)
+            if ((boardArray[cellA].textContent === boardArray[cellB].textContent) && (boardArray[cellA].textContent === boardArray[cellC].textContent)) {
+
+                roundWon === true;
+                console.log('winner', currentPlayer)
+                playerChange()
+                break;
+            }
+    // }
+}
+        if(roundWon){
+        statusText.textContent = `${currentPlayer}wins!`
+        running = false
+        }
+        else if(!selection.includes('')){
+        statusText.textContent = 'Draw!'
+        running = false
+        }
+        else{
+            // checkForWinner()
+        }
 
 }
 
@@ -86,6 +99,7 @@ function playerChange(){
     
     currentPlayer = (currentPlayer == 'X') ? "O" : "X"
     statusText.textContent = `${currentPlayer}'s turn`
+    
 }
 
 
@@ -94,10 +108,10 @@ function restartGame(){
     let selection = ["", "", "", "", "", "", "", "", ""]
     statusText.textContent = `${currentPlayer}'s turn`
     cells.forEach(cell => cell.textContent = '')
+    statusText.classList.add("d-none")
 
 }
 
-
-
-
+//Things I would do differently: Don't spend as much time on style initially. 
+//Get basic elements in place but ultimately focus on functionality first. 
 
