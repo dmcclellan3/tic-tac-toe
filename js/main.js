d7c30c00d13936e76a7a6195b2e34c755f8c0bca
dmcclellan3 <wdmcclellan3@gmail.com>
2024-04-19 09:09:44 -0400

    save

--- js/main.js
+++ js/main.js
@@ -1,127 +1,12 @@
-// Bringing over elements that are needed
-
-// const cells = document.querySelectorAll('.cell')
-// const restartBtn = document.querySelector('#restart-button')
-// const statusText = document.querySelector('#status-text')
-// const boardArray = document.querySelectorAll('.cell')
-// boardArray.forEach(cell => cell.addEventListener('click', updateCell))
-// document.querySelector('#restart-button').addEventListener('click', restartGame) 
-// console.log('boardArray', boardArray)
-
-
-// Listing out the items in the array for all possible ways to win. 
-
-// const winConditions = [
-//         [0, 1, 2],
-//         [3, 4, 5],
-//         [6, 7, 8],
-//         [0, 3, 6],
-//         [1, 4, 7],
-//         [2, 5, 8],
-//         [0, 4, 8],
-//         [2, 4, 6],
-// ]
-
-
-// let currentPlayer = "X"
-// let selection = ["", "", "", "", "", "", "", "", ""]
-// let running = true
-
-//all the functions needed for the game
-
-
-
-// function startGame(){
-
-//     document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', updateCell))
-//     document.querySelector('restart-button').addEventListener('click', restartGame) 
-//     statusText.textContent = `${currentPlayer}s turn`
-//     running = true 
-
-// }
-
-
-
-//closest finds what element we are in.  Whatever was clicked give us the closest parent that has a cell class. 
-// function updateCell(event){
-
-//     const cell = event.target.closest('.cell')
-//     if (cell.textContent === '') {
-//         cell.textContent = currentPlayer
-//         statusText.classList.remove("d-none")
-        
-//         checkForWinner()
-//         playerChange()
-//     }
-    
-// }
-    
-// function checkForWinner(){
-    
-//     let roundWon = false
-//     for (let i = 0; i < winConditions.length; i++){
-//         const cellA = winConditions[i][0]
-//         const cellB = winConditions[i][1]
-//         const cellC = winConditions[i][2]
-        
-//         if(boardArray[cellA].textContent === '' || boardArray[cellB].textContent === '' || boardArray[cellC].textContent === ''){
-//             continue;
-//         }
-        
-//             if ((boardArray[cellA].textContent === boardArray[cellB].textContent) && (boardArray[cellA].textContent === boardArray[cellC].textContent)) {
-
-//                 roundWon = true;
-//                 statusText.textContent = `${currentPlayer}wins!`
-//                 console.log('winner', currentPlayer)
-//                 running = false
-//                 break;
-//             }
-
-            
-    // }
-// }
-        // if(roundWon){
-        // statusText.textContent = `${currentPlayer}wins!`
-        // running = false
-        // }
-        // else if(!selection.includes('')){  //if there are no options or spaces remaining 
-        // statusText.textContent = 'Draw!'   //then we can use the not operator to indicate a draw
-        // running = false
-        
-        // }
-        // else{
-        //     // playerChange()
-        // }
-
-// }
-
-// function playerChange(){
-    
-//     currentPlayer = (currentPlayer == 'X') ? "O" : "X"
-//     statusText.textContent = `${currentPlayer}'s turn`
-    
-// }
-
-
-// function restartGame(){
-//     let currentPlayer = 'X'
-//     let selection = ["", "", "", "", "", "", "", "", ""]
-//     statusText.textContent = `${currentPlayer}'s turn`
-//     cells.forEach(cell => cell.textContent = '')
-//     statusText.classList.add("d-none")
-
-// }
-
-//Things I would do differently: Don't spend as much time on style initially. 
-//Get basic elements in place but ultimately focus on functionality first. 
-
 const cells = document.querySelectorAll('.cell')
 const restartBtn = document.querySelector('.restart-button')
 const statusText = document.querySelector('#status-text')
 const boardArray = document.querySelectorAll('.cell')
 let currentPlayer = 'X'
+let running = false
+let roundWon = false
 
-const winConditions = [
+const winConditions = [  //winning conditions for the game placed in an array. 
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
@@ -132,59 +17,78 @@ const winConditions = [
         [2, 4, 6],
 ]
 
+let selection = ["", "", "", "", "", "", "", "", ""] //array to track spaces that are picked
+
 // Initialize game
 startGame();
 
 
-// Event listeners
+// Event listener
 restartBtn.addEventListener('click', restartGame);
 
 function startGame() {
-    document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', updateCell))
+    document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', updateCell))  //event listener for the cells
     statusText.textContent = `${currentPlayer}'s turn`
 }
 
-function updateCell(event) {
-    const cell = event.target.closest('.cell')
+function updateCell(event, index) {   //
+    if (roundWon) return;
+    selection[index] = currentPlayer
+    const cell = event.target.closest('.cell')  
     if (cell.textContent === '') {
         cell.textContent = currentPlayer
-        checkForWinner()
         playerChange()
+        checkForWinner()
+    }
+}
+
+function showWinner() {
+    if (currentPlayer === "X") {
+        statusText.textContent = 'O wins!'  //Had to hardcode due to issue. X's or O's would win but the game would say that the opposite player won. 
+    }
+    if(currentPlayer === "O"){
+        statusText.textContent = 'X wins!'
     }
 }
 
 function checkForWinner() {
-    let roundWon = false
-    for (let i = 0; i < winConditions.length; i++) {
+    roundWon = false
+    for (let i = 0; i < winConditions.length; i++) {  //
         const cellA = winConditions[i][0]
         const cellB = winConditions[i][1]
         const cellC = winConditions[i][2]
-
+        
         if (boardArray[cellA].textContent === '' || boardArray[cellB].textContent === '' || boardArray[cellC].textContent === '') {
-            continue;
+            continue; //if there hasn't been an X or O found in the win conditions in the loop the game continues
         }
-
+        
         if ((boardArray[cellA].textContent === boardArray[cellB].textContent) && (boardArray[cellA].textContent === boardArray[cellC].textContent)) {
             roundWon = true;
-            statusText.textContent = `${currentPlayer} wins!`
+            showWinner()
             running = false
-            break;
+            break; //If there has been an X's or O's found in the wining conditions it declares the winner and breaks out of the loop. 
         }
     }
-
+    
     if (!roundWon && !Array.from(boardArray).some(cell => cell.textContent === '')) {
-        statusText.textContent = 'Draw!'
+        statusText.textContent = 'Draw!'  //If there hasn't been a player win the game and there isn't any spaces left the game is a draw. 
         running = false
-    }
+    } 
+    
 }
 
 function playerChange() {
-    currentPlayer = (currentPlayer == 'X') ? "O" : "X"
-    statusText.textContent = `${currentPlayer}'s turn`
+    currentPlayer = (currentPlayer == 'X') ? "O" : "X"  //ternary operator allows the player to change 
+    statusText.textContent = `${currentPlayer}'s turn` //template literal places the players turn in the text content
 }
 
 function restartGame() {
     currentPlayer = 'X'
-    statusText.textContent = `${currentPlayer}'s turn`
+    statusText.textContent = `${currentPlayer}'s turn`  //resets game and erases all cells. 
     cells.forEach(cell => cell.textContent = '')
+    roundWon = false
 }
+
+//Things I would do differently: Don't spend as much time on style initially. 
+//Get basic elements in place but ultimately focus on functionality first.
+//Take more breaks.  I look at things for too long and burn out 
