f04ea525869dcbc72408f49ef24d5e3227ae2f5a
dmcclellan3 <wdmcclellan3@gmail.com>
2024-04-19 09:55:13 -0400

    saving-final

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const cells = document.querySelectorAll('.cell')
-const restartBtn = document.querySelector('.restart-button')
+const restartBtn = document.querySelector('.restart-button')  //bringing over elements from HTML and declaring variables
 const statusText = document.querySelector('#status-text')
 const boardArray = document.querySelectorAll('.cell')
 let currentPlayer = 'X'
@@ -17,7 +17,7 @@ const winConditions = [  //winning conditions for the game placed in an array.
         [2, 4, 6],
 ]
 
-let selection = ["", "", "", "", "", "", "", "", ""] //array to track spaces that are picked
+let selection = ["", "", "", "", "", "", "", "", ""] //array to track spaces that are picked  
 
 // Initialize game
 startGame();
@@ -26,15 +26,19 @@ startGame();
 // Event listener
 restartBtn.addEventListener('click', restartGame);
 
+//event listener for the cells    
+
 function startGame() {
-    document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', updateCell))  //event listener for the cells
+    document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', updateCell))  
     statusText.textContent = `${currentPlayer}'s turn`
 }
 
-function updateCell(event, index) {   //
+function updateCell(event, index) {   //the function updates the index of current selections and stores it, 
+                                        //listens for clicks on the board, and if there is an empty space it allows for the current player to select it. 
+                                        //after each click it calls the functions to change the player while checking for a winner. 
     if (roundWon) return;
     selection[index] = currentPlayer
-    const cell = event.target.closest('.cell')  
+    const cell = event.target.closest('.cell')  //
     if (cell.textContent === '') {
         cell.textContent = currentPlayer
         playerChange()
@@ -53,7 +57,7 @@ function showWinner() {
 
 function checkForWinner() {
     roundWon = false
-    for (let i = 0; i < winConditions.length; i++) {  //
+    for (let i = 0; i < winConditions.length; i++) {  //loops through the array defined previously to check for winning conditions
         const cellA = winConditions[i][0]
         const cellB = winConditions[i][1]
         const cellC = winConditions[i][2]
@@ -68,14 +72,17 @@ function checkForWinner() {
             running = false
             break; //If there has been an X's or O's found in the wining conditions it declares the winner and breaks out of the loop. 
         }
-    }
     
-    if (!roundWon && !Array.from(boardArray).some(cell => cell.textContent === '')) {
+    
+        if (!roundWon && !Array.from(boardArray).some(cell => cell.textContent === '')) {
         statusText.textContent = 'Draw!'  //If there hasn't been a player win the game and there isn't any spaces left the game is a draw. 
         running = false
-    } 
+        } 
+
+    }
     
 }
+    
 
 function playerChange() {
     currentPlayer = (currentPlayer == 'X') ? "O" : "X"  //ternary operator allows the player to change 
@@ -92,3 +99,4 @@ function restartGame() {
 //Things I would do differently: Don't spend as much time on style initially. 
 //Get basic elements in place but ultimately focus on functionality first.
 //Take more breaks.  I look at things for too long and burn out 
+//additional pseudocoding
